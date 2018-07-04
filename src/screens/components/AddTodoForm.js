f195ff11b54050189c707cb3832a708c74197ff2
Weslley Nascimento Rocha <weslleytato.12@gmail.com>
2018-07-03 21:55:24 -0300

    Finished custom components

--- src/screens/components/AddTodoForm.js
+++ src/screens/components/AddTodoForm.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-import { View, Keyboard } from "react-native";
+import { View, Keyboard, StyleSheet, TouchableOpacity } from "react-native";
 
 import moment from "../../util/moment";
 
-import {
-  Button,
-  Text,
-  Icon,
-  Form,
-  Input,
-  Item,
-  Card
-} from "native-base";
+import { Text, Form, Input, Item, Card, Label } from "native-base";
 
 import Toast from "react-native-root-toast";
 import Datepicker from "react-native-datepicker";
@@ -31,7 +23,40 @@ class AddTodoForm extends Component {
   }
 
   setDate(newDate) {
-    this.setState({ date: moment(newDate,"DD/MM/YYYY")});
+    this.setState({ date: moment(newDate, "DD/MM/YYYY") });
+  }
+
+  handleAdd() {
+    const { title, date } = this.state;
+    Keyboard.dismiss();
+    if (title === "") {
+      const toast = Toast.show("Title cannot be empty", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0
+      });
+    } else if (date === null) {
+      const toast = Toast.show("Date cannot be empty", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0
+      });
+    } else {
+      this.setState({
+        title: "",
+        date: null
+      });
+      this.props.addTodo({
+        title,
+        date: moment(date, "DD/MM/YYYY").toISOString()
+      });
+    }
   }
 
   render() {
@@ -39,67 +64,46 @@ class AddTodoForm extends Component {
     return (
       <Card>
         <View style={{ marginVertical: 20, marginHorizontal: 5 }}>
+          <Text style={styles.title}>ADD A TODO</Text>
           <Form>
-            <Item>
+            <Item style={styles.container}>
+              <Text>Todo name</Text>
               <Input
                 placeholder="Create a new todo item"
                 onChangeText={title => this.setState({ title })}
                 value={title}
               />
             </Item>
-            <View style={{ marginTop: 20, marginLeft: 15, flex: 1, flexDirection: "row" }}>
-              <View style={{ flex: 1 }}>
-                <Datepicker
-                  date={date ? moment(date).format("DD/MM/YYYY") : ""}
-                  mode="date"
-                  placeholder="Please select a date"
-                  format="DD/MM/YYYY"
-                  minDate={new Date()}
-                  confirmBtnText="Confirm"
-                  cancelBtnText="Cancel"
-                  onDateChange={(date) => {
-                    console.log(date);
-                    this.setDate(date)
-                  }}
-                  showIcon={false}
-                  style={{ width: 250 }}
-                />
-              </View>
-              <Button
-                iconRight
-                style={{ flex: 0 }}
-                onPress={() => {
-                  Keyboard.dismiss();
-                  if (title === "") {
-                    const toast = Toast.show("Title cannot be empty", {
-                      duration: Toast.durations.SHORT,
-                      position: Toast.positions.BOTTOM,
-                      shadow: true,
-                      animation: true,
-                      hideOnPress: true,
-                      delay: 0
-                    });
-                  } else if (date === null) {
-                    const toast = Toast.show("Date cannot be empty", {
-                      duration: Toast.durations.SHORT,
-                      position: Toast.positions.BOTTOM,
-                      shadow: true,
-                      animation: true,
-                      hideOnPress: true,
-                      delay: 0
-                    });
-                  } else {
-                    this.setState({
-                      title: "",
-                      date: null
-                    });
-                    this.props.addTodo({ title, date: moment(date,"DD/MM/YYYY").toISOString() });
-                  }
+            <Item style={styles.container}>
+              <Label>Todo date</Label>
+              <Datepicker
+                date={date ? moment(date).format("DD/MM/YYYY") : ""}
+                mode="date"
+                placeholder="Please select a date"
+                format="DD/MM/YYYY"
+                minDate={new Date()}
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                onDateChange={date => {
+                  console.log(date);
+                  this.setDate(date);
                 }}
+                showIcon={false}
+                customStyles={styleDatepicker}
+                allowFontScaling
+                style={{ width: "100%" }}
+              />
+            </Item>
+            <View style={styles.container}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.handleAdd();
+                }}
+                color="#27ae60"
+                style={styles.button}
               >
-                <Text>Create</Text>
-                <Icon name="add-circle" />
-              </Button>
+                <Text style={styles.buttonText}>Add</Text>
+              </TouchableOpacity>
             </View>
           </Form>
         </View>
@@ -112,3 +116,39 @@ export default connect(
   null,
   { addTodo }
 )(AddTodoForm);
+
+export const styleDatepicker = {
+  placeholderText: {
+    fontSize: 16,
+    color: "black"
+  },
+  dateInput: {
+    padding: 0,
+    alignItems: "flex-start"
+  },
+  dateText: {
+    fontSize: 18
+  }
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#27ae60",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25
+  },
+  buttonText: { color: "#fff", fontWeight: "bold" },
+  container: {
+    marginBottom: 20,
+    marginHorizontal: 15
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginLeft: 10,
+    marginBottom: 15
+  }
+});
--- src/screens/components/EditTodoForm.js
+++ src/screens/components/EditTodoForm.js
@@ -1,104 +1,114 @@
 import React, { Component } from "react";
-import { View, Keyboard, Switch } from "react-native";
+import {
+  View,
+  Keyboard,
+  Switch,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 import moment from "../../util/moment";
 
-import {
-  Button,
-  Text,
-  Icon,
-  Form,
-  Input,
-  Item,
-  Card
-} from "native-base";
+import { Text, Icon, Form, Input, Item, Card, Label } from "native-base";
 
 import Toast from "react-native-root-toast";
 import Datepicker from "react-native-datepicker";
 
 class EditTodoForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.setDate = this.setDate.bind(this);
-    const { title, date, isCompleted, id } = this.props.todo
+    const { title, date, isCompleted, id } = this.props.todo;
     this.state = {
       title: title,
       date: moment(date).format("DD/MM/YYYY"),
       isCompleted: isCompleted,
       id: id
-    };    
+    };
   }
 
   setDate(newDate) {
     this.setState({ date: moment(newDate, "DD/MM/YYYY") });
   }
 
+  handleUpdate() {
+    const { title, date } = this.state;
+    Keyboard.dismiss();
+    if (title === "") {
+      const toast = Toast.show("Title cannot be empty", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0
+      });
+    } else if (date === null) {
+      const toast = Toast.show("Date cannot be empty", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0
+      });
+    } else {
+      this.props.update(this.state);
+    }
+  }
 
   render() {
-    const { title, date, isCompleted } = this.state
+    const { title, date, isCompleted } = this.state;
     return (
       <Card>
         <View style={{ marginVertical: 20, marginHorizontal: 5 }}>
           <Form>
-            <Item>
+            <Item style={styles.container}>
+              <Label>Todo name: </Label>
               <Input
                 onChangeText={title => this.setState({ title })}
                 value={title}
               />
             </Item>
-            <View style={{ marginTop: 20, marginLeft: 15, flex: 1, flexDirection: "row" }}>
+            <Item style={styles.container}>
+              <Label>Todo date: </Label>
               <View style={{ flex: 1 }}>
                 <Datepicker
-                  date={date ? moment(date,"DD/MM/YYYY") : ""}
+                  date={date ? moment(date, "DD/MM/YYYY") : ""}
                   mode="date"
                   placeholder="Please select a date"
                   format="DD/MM/YYYY"
-                  minDate={moment("L","DD/MM/YYYY").toString()}
+                  minDate={moment("L", "DD/MM/YYYY").toString()}
                   confirmBtnText="Confirm"
                   cancelBtnText="Cancel"
-                  onDateChange={(date) => {
+                  onDateChange={date => {
                     console.log(date);
-                    this.setDate(date)
+                    this.setDate(date);
                   }}
                   showIcon={false}
                   style={{ width: 250 }}
                 />
               </View>
-              <View>
-                <Switch value={isCompleted} onValueChange={()=>this.setState({isCompleted:!isCompleted})} />
-              </View>
-              <Button
-                iconRight
-                style={{ flex: 0 }}
+            </Item>
+            <Item style={styles.container}>
+              <Label>Is completed?</Label>
+              <Switch
+                value={isCompleted}
+                onValueChange={() =>
+                  this.setState({ isCompleted: !isCompleted })
+                }
+              />
+            </Item>
+            <View style={styles.container}>
+              <TouchableOpacity
                 onPress={() => {
-                  Keyboard.dismiss();
-                  if (title === "") {
-                    const toast = Toast.show("Title cannot be empty", {
-                      duration: Toast.durations.SHORT,
-                      position: Toast.positions.BOTTOM,
-                      shadow: true,
-                      animation: true,
-                      hideOnPress: true,
-                      delay: 0
-                    });
-                  } else if (date === null) {
-                    const toast = Toast.show("Date cannot be empty", {
-                      duration: Toast.durations.SHORT,
-                      position: Toast.positions.BOTTOM,
-                      shadow: true,
-                      animation: true,
-                      hideOnPress: true,
-                      delay: 0
-                    });
-                  } else {
-                    console.log(this.state);                    
-                    this.props.update(this.state);
-                  }
+                  this.handleUpdate();
                 }}
+                color="#27ae60"
+                style={styles.button}
               >
-                <Text>Save</Text>
-                <Icon name="add-circle" />
-              </Button>
+                <Text style={styles.buttonText}>Save</Text>
+              </TouchableOpacity>
             </View>
           </Form>
         </View>
@@ -108,3 +118,18 @@ class EditTodoForm extends Component {
 }
 
 export default EditTodoForm;
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#27ae60",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25
+  },
+  buttonText: { color: "#fff", fontWeight: "bold" },
+  container: {
+    marginBottom: 20,
+    marginHorizontal: 15
+  }
+});
